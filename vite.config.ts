555269aa80983fe1354fe4e27aae5bfa55b5844a
varunishad <72733486+varunishad@users.noreply.github.com>
2026-01-02 21:25:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADD THIS LINE BELOW
+  base: '/jaizek-moves-made-easy/', 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
